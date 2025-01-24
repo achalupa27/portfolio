e223d50e7bc4052494111f9bd3e6ef0244cfe09b
Andrew Chalupa <andrewchalupa@pm.me>
2025-01-24 17:26:33 -0500

    no-scroll basic non-functioning theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,61 +3,59 @@ module.exports = {
     darkMode: ['class'],
     content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './themes.ts', './src/components/**/*.{js,ts,jsx,tsx}'],
     theme: {
-    	fontFamily: {
-    		sans: [
-    			'Inter'
-    		]
-    	},
-    	extend: {
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		},
-    		colors: {},
-    		animation: {
-    			meteor: 'meteor 5s linear infinite',
-    			gradient: 'gradient 8s linear infinite',
-    			'accordion-down': 'accordion-down 0.2s ease-out',
-    			'accordion-up': 'accordion-up 0.2s ease-out'
-    		},
-    		keyframes: {
-    			meteor: {
-    				'0%': {
-    					transform: 'rotate(215deg) translateX(0)',
-    					opacity: '1'
-    				},
-    				'70%': {
-    					opacity: '1'
-    				},
-    				'100%': {
-    					transform: 'rotate(215deg) translateX(-500px)',
-    					opacity: '0'
-    				}
-    			},
-    			gradient: {
-    				to: {
-    					backgroundPosition: 'var(--bg-size) 0'
-    				}
-    			},
-    			'accordion-down': {
-    				from: {
-    					height: '0'
-    				},
-    				to: {
-    					height: 'var(--radix-accordion-content-height)'
-    				}
-    			},
-    			'accordion-up': {
-    				from: {
-    					height: 'var(--radix-accordion-content-height)'
-    				},
-    				to: {
-    					height: '0'
-    				}
-    			}
-    		}
-    	}
+        fontFamily: {
+            sans: ['Inter'],
+        },
+        extend: {
+            borderRadius: {
+                lg: 'var(--radius)',
+                md: 'calc(var(--radius) - 2px)',
+                sm: 'calc(var(--radius) - 4px)',
+            },
+            colors: {},
+            animation: {
+                meteor: 'meteor 5s linear infinite',
+                gradient: 'gradient 8s linear infinite',
+                'accordion-down': 'accordion-down 0.2s ease-out',
+                'accordion-up': 'accordion-up 0.2s ease-out',
+            },
+            keyframes: {
+                meteor: {
+                    '0%': {
+                        transform: 'rotate(215deg) translateX(0)',
+                        opacity: '1',
+                    },
+                    '70%': {
+                        opacity: '1',
+                    },
+                    '100%': {
+                        transform: 'rotate(215deg) translateX(-500px)',
+                        opacity: '0',
+                    },
+                },
+                gradient: {
+                    to: {
+                        backgroundPosition: 'var(--bg-size) 0',
+                    },
+                },
+                'accordion-down': {
+                    from: {
+                        height: '0',
+                    },
+                    to: {
+                        height: 'var(--radix-accordion-content-height)',
+                    },
+                },
+                'accordion-up': {
+                    from: {
+                        height: 'var(--radix-accordion-content-height)',
+                    },
+                    to: {
+                        height: '0',
+                    },
+                },
+            },
+        },
     },
-    plugins: [require('tailwind-scrollbar'), require('tailwindcss-animate')],
+    plugins: [require('tailwindcss-animate')],
 };
